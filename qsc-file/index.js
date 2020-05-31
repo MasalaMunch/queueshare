714d0a46ce90b7fea88dc9627879a0e15593fbab
MasalaMunch <rsdean@wisc.edu>
2020-05-31 15:02:31 -0500

    html templating

--- qsc-file/index.js
+++ qsc-file/index.js
@@ -1,9 +1,38 @@
 "use strict";
 
+const fs = require(`fs`);
+const mustache = require(`mustache`);
 const path = require(`path`);
+const stringFileEncoding = require(`../string-file-encoding`);
 
-const File = (isDev) => path.join(__dirname, `file.html`);
+const apiPaths = require(`../qss-api-paths`);
 
-//TODO if isDev, compile the file from source modules
+const file = path.join(__dirname, `file.html`);
+
+const File = (isDev) => {
+
+    if (isDev) {
+
+        const templateContents = fs.readFileSync(
+
+            path.join(__dirname, `template.html`),
+
+            {encoding: stringFileEncoding},
+
+            );
+
+        const newFileContents = mustache.render(templateContents, {
+
+            assetsUrl: apiPaths.clientAssets,
+
+            });
+
+        fs.writeFileSync(file, newFileContents, {encoding: stringFileEncoding});
+
+    }
+
+    return file;
+
+};
 
 module.exports = File;
