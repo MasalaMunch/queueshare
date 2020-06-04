b5195d9895f42c922d6d147205ae7a60bc0c5974
MasalaMunch <rsdean@wisc.edu>
2020-06-04 14:14:32 -0500

    progress

--- qsp-file/index.js
+++ qsp-file/index.js
@@ -1,12 +1,9 @@
 "use strict";
 
-const fs = require(`fs`);
 const path = require(`path`);
 
 const folder = require(`../qsp-folder`);
 
 const file = path.join(folder, `package.json`);
 
-const ModTime = async () => (await fs.promises.stat(file)).mtimeMs;
-
-module.exports = ModTime;
+module.exports = file;
--- update-qsp/index.js
+++ update-qsp/index.js
@@ -3,13 +3,16 @@
 const execa = require(`execa`);
 const path = require(`path`);
 
+const JsonPromise = require(`../qsp-json-promise`);
 const folder = require(`../qsp-folder`);
 
 const parentPackageFolder = path.resolve(folder, `..`, `..`);
 
-const update = () => {
+const update = async () => {
 
-    return execa(`npm`, [`update`, `queueshare`], {cwd: parentPackageFolder});
+    const {name} = await JsonPromise();
+
+    await execa(`npm`, [`update`, name], {cwd: parentPackageFolder});
 
 };
 
