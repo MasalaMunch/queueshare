4a262f602af00ac9df04c79a19d534641326543d
MasalaMunch <rsdean@wisc.edu>
2020-06-09 13:30:46 -0500

    deprecated stored json log via local storage because it has unresolved issues with multiple tabs writing the same data and the plan is to now just not store synced state on local storage

--- stored-json-log/index.js
+++ stored-json-log/index.js
@@ -1,8 +1,76 @@
 "use strict";
 
-const ViaFs = require(`./ViaFs.js`);
-const ViaLocalStorage = require(`./ViaLocalStorage.js`);
+const FileContents = require(`../file-contents`);
+const fs = require(`fs`);
+const JsonString = require(`../json-string`);
+const stringFileEncoding = require(`../string-file-encoding`);
 
-const StoredJsonLog = ViaFs.IsSupported()? ViaFs : ViaLocalStorage;
+const jsonStringSeparator = `\n`;
+
+const StoredJsonLog = class {
+
+    constructor (storagePath) {
+
+        this._file = storagePath;
+
+        this._fileAppendStream = undefined;
+
+    }
+
+    Entries () {
+
+        const fileContents = FileContents(
+
+            this._file, 
+
+            {encoding: stringFileEncoding},
+
+            );
+
+        const fileAsString = fileContents === undefined? `` : fileContents;
+
+        return (
+
+            fileAsString.split(jsonStringSeparator).slice(0, -1).map(JSON.parse)
+
+            );
+
+    }
+
+    eventuallyAppend (entry) {
+
+        if (this._fileAppendStream === undefined) {
+
+            this._fileAppendStream = fs.createWriteStream(
+
+                this._file, 
+
+                {encoding: stringFileEncoding, flags: `a`},
+
+                );
+
+        }
+
+        this._fileAppendStream.write(JsonString(entry) + jsonStringSeparator);
+
+    }
+
+    write (entries) {
+
+        const jsonStrings = entries.map(JsonString);
+
+        fs.writeFileSync(
+
+            this._file, 
+
+            jsonStrings.join(jsonStringSeparator) + jsonStringSeparator,
+
+            {encoding: stringFileEncoding},
+
+            );
+
+    }
+
+    };
 
 module.exports = StoredJsonLog;
