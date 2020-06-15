1864dab64eb91eaba2b040bafe58d62dece38cc2
MasalaMunch <rsdean@wisc.edu>
2020-06-15 10:49:52 -0500

    fun synced state change logging to frontend

--- qsc-asset-folder-promise/folder/browserifiedScript.js
+++ qsc-asset-folder-promise/folder/browserifiedScript.js
@@ -3502,33 +3502,29 @@ module.exports = PortableUuid;
 const Elm = require(`../elm`);
 
 const keepUpdated = require(`../keep-qsc-updated`);
-const SyncedState = require(`../synced-qsc-state`);
+const syncedState = require(`../synced-qsc-state`);
 
 keepUpdated();
 
-const syncedState = new SyncedState();
-
 window.ss = syncedState;
 
-const contentElm = Elm(`div`, {className: `content`, innerText: `
-
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sagittis venenatis nibh sollicitudin interdum. Vivamus finibus ex leo, eu vehicula arcu pulvinar quis. Morbi ut iaculis arcu. Fusce blandit elit id odio accumsan, consectetur semper justo elementum. Morbi viverra eros in justo suscipit tristique. Morbi in augue vel leo malesuada rhoncus. Donec lorem massa, maximus sed sollicitudin sit amet, volutpat eget massa. Donec tincidunt auctor lacus, in fermentum ante consequat eu. Ut blandit massa et urna finibus, ac molestie magna lacinia. Integer pulvinar, mi lobortis eleifend volutpat, turpis lacus accumsan elit, nec ornare ante turpis eu libero. Duis pharetra magna dolor, ac ultrices justo vestibulum id. Nam mattis eget dolor sed tempus. Phasellus fringilla massa magna, a tincidunt orci congue sed.
-
-    In eros urna, feugiat in lacus ut, vulputate mollis quam. Nullam pellentesque sagittis luctus. Duis placerat turpis sit amet arcu faucibus, quis tempor metus auctor. Vivamus ac urna convallis, pulvinar dui a, feugiat est. Nam vehicula egestas iaculis. Nulla arcu nunc, ornare id tristique vel, ornare et tellus. Ut at tincidunt libero. Morbi eget mauris id dolor volutpat semper quis ac nisi. Vivamus interdum fringilla quam ut placerat.
-
-    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse id volutpat nunc. Integer tristique lorem eu libero suscipit sagittis. Donec vel libero et nisi ultrices porttitor ut eget leo. Nam sit amet erat lectus. Proin pulvinar ex sit amet orci convallis dictum. Fusce lobortis urna est, ac varius urna consectetur sit amet. Donec vulputate non enim consectetur pulvinar. Sed porttitor quam ut quam accumsan, vitae cursus mi feugiat. Mauris sed erat non purus hendrerit convallis. Pellentesque tincidunt lacinia felis id ornare. Proin vitae mauris velit. Sed posuere, nunc nec interdum dictum, lectus neque luctus nisi, molestie semper urna risus et neque. Ut est ipsum, tincidunt vitae interdum congue, viverra eget felis.
-
-    Duis posuere lobortis tortor, non egestas purus posuere eget. Donec sollicitudin luctus tortor at ultricies. Aliquam quis nunc ut nulla semper ullamcorper vel eget orci. Sed consequat molestie nibh sed fringilla. Quisque pharetra suscipit ipsum, quis rhoncus tellus fringilla at. Sed sed eleifend eros. Cras vitae aliquet est, in egestas leo. Aliquam tincidunt sit amet arcu dignissim dapibus.
-
-    Suspendisse nec est id dolor accumsan sagittis condimentum at nisi. Nam dapibus metus eget massa rhoncus commodo interdum ac elit. Phasellus tempor lectus at vestibulum imperdiet. Maecenas ut libero tincidunt, porta mauris a, aliquam tortor. Praesent nec sem elementum, fermentum justo id, commodo metus. Praesent sagittis sagittis nunc, sed suscipit magna tristique vel. Nullam eleifend, turpis at volutpat sollicitudin, tortor lectus semper purus, in tincidunt nunc nulla in turpis. Praesent efficitur tempus ante, eu porttitor erat aliquam consequat. Nullam justo nulla, tempor et commodo a, lacinia sed neque. Ut tempor orci pretium, ornare eros molestie, semper ex. Donec semper eget tellus sit amet ultrices. Donec ut dolor vel est consequat efficitur.
-
-    `});
+const contentElm = Elm(`code`, {className: `content`, childNodes: [Elm(`pre`)]});
 
 document.body.appendChild(contentElm);
 
 const fabElm = Elm(`div`, {className: `fab`, childNodes: [Elm(`button`)]});
 
 document.body.appendChild(fabElm);
+
+syncedState._syncedJsonTree.events.on(`change`, (c) => {
+
+    contentElm.querySelector(`pre`).appendChild(
+
+        document.createTextNode(JSON.stringify(c, undefined, 4) + `\n\n`
+
+        ));
+
+});
 
 },{"../elm":2,"../keep-qsc-updated":8,"../synced-qsc-state":48}],41:[function(require,module,exports){
 "use strict";
@@ -4069,6 +4065,8 @@ const SyncedState = class {
 
     };
 
-module.exports = SyncedState;
+const syncedState = new SyncedState();
+
+module.exports = syncedState;
 
 },{"../eventually":3,"../interval":6,"../json-fetch":7,"../qsc-change-delay":41,"../qsc-is-dev":42,"../qss-api-paths":43,"../synced-json-tree":47,"querystring":22}]},{},[40]);
